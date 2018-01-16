4733e89cfd74e8768427bf70fd7b547819b2829b
edu <edu@bb8.minwi.lan>
2018-01-16 12:56:39 +0100

    Disabled rescue to save space

--- dracut.sh
+++ dracut.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo 'add_drivers+="pwm-lpss pwm-lpss-platform i915"' | sudo tee -a /etc/dracut.conf.d/gpd_pocket.conf
+echo 'dracut_rescue_images="no"' | sudo tee -a /etc/dracut.conf.d/gpd_pocket.conf
 
 echo "Run:"
 echo "sudo dracut -f /boot/initramfs-$(uname -r).img $(uname -r)"
--- fix_kernel.sh
+++ fix_kernel.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo sed -ie 's/^UPDATEDEFAULT=.*/UPDATEDEFAULT=no/' /etc/sysconfig/kernel
+sudo rm -f /boot/*rescue*
