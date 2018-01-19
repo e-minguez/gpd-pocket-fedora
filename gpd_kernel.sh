33a06773dc6aa05ebafa90d1937484532c276b41
Eduardo Minguez <edu@redhat.com>
2018-01-19 13:23:08 +0100

    Proper fedora version

--- gpd_kernel.sh
+++ gpd_kernel.sh
@@ -5,5 +5,5 @@ git reset --hard origin/master
 COMMITID=$(git log --format="%h" -n 1)
 make clean
 make -j $(getconf _NPROCESSORS_ONLN) binrpm-pkg LOCALVERSION=-${COMMITID}-gpd-custom
-package_cloud push eminguez/gpd-pocket-kernel ${HOME}/rpmbuild/RPMS/x86_64/kernel-*
+package_cloud push eminguez/gpd-pocket-kernel/fedora/27 ${HOME}/rpmbuild/RPMS/x86_64/kernel-*
 rm -f ${HOME}/rpmbuild/RPMS/x86_64/kernel-*
