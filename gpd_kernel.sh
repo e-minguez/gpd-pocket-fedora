d9523932bd84e23cc093e59717f7e87912bafca8
Eduardo Minguez Perez <e.minguez@gmail.com>
2018-06-21 11:03:29 +0200

    Added .config files for reference and tune kernel for atom

--- gpd_kernel.sh
+++ gpd_kernel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # rpm-pkg    - Build both source and binary RPM kernel packages
 # binrpm-pkg - Build only the binary kernel package
-FEDORAVERSION="27"
+FEDORAVERSION="28"
 PKGTYPE="rpm-pkg"
+VERSION="gpd-custom-atom"
 
 cd ${HOME}/git/gpd-pocket-kernel
 git fetch --all
@@ -10,6 +11,15 @@ git reset --hard origin/master
 git clean -dfx
 COMMITID=$(git log --format="%h" -n 1)
 make clean
-make -j $(getconf _NPROCESSORS_ONLN) ${PKGTYPE} LOCALVERSION=-${COMMITID}-gpd-custom
-package_cloud push eminguez/gpd-pocket-kernel/fedora/${FEDORAVERSION} ${HOME}/rpmbuild/RPMS/x86_64/kernel-*
-echo "Don't forget to clean the rpms..."
+sed -i \
+  -e 's/# CONFIG_MATOM is not set/CONFIG_MATOM=y/g' \
+  -e 's/CONFIG_GENERIC_CPU=y/# CONFIG_GENERIC_CPU is not set/g' \
+  -e 's/# CONFIG_BACKLIGHT_PWM is not set/CONFIG_BACKLIGHT_PWM=m/g' \
+  -e 's/# CONFIG_LEDS_PWM is not set/CONFIG_LEDS_PWM=m/g' \
+  ${HOME}/git/gpd-pocket-kernel/.config
+make -j $(getconf _NPROCESSORS_ONLN) ${PKGTYPE} LOCALVERSION=-${COMMITID}-${VERSION}
+for package in ${HOME}/rpmbuild/RPMS/x86_64/kernel-*
+do
+  package_cloud push eminguez/gpd-pocket-kernel/fedora/${FEDORAVERSION} ${package} && rm -f ${package}
+done
+rpmdev-wipetree
